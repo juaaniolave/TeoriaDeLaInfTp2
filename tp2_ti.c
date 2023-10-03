d60c235c190a6ab4d73fd9fdda7ddff43594382c
juaaniolave <juaaniolave@gmail.com>
2023-10-03 10:46:25 -0300

    C terminado

--- tp2_ti.c
+++ tp2_ti.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX_PALABRA 50
 #define MAX_ALFABET 256
@@ -39,7 +40,8 @@ void lee_archivo (char *nombre_archivo) {
    int i, j, k, m, longitud;
    int encontrada;
    int alfabeto[MAX_ALFABET] = {0}; //inicializa todas las componentes en 0
-
+   double entropia=0;
+   double longitudMedia=0;
    FILE* archivo = fopen(nombre_archivo,"rt");
 
    if (archivo == NULL){
@@ -47,7 +49,7 @@ void lee_archivo (char *nombre_archivo) {
       exit (-1);
    }
 
-   while (fscanf(archivo, "%s", palabra)) {
+   while (fscanf(archivo, "%s", palabra) == 1) {
       encontrada = 0;
       // Busca la palabra en el array
       for (i = 0 ; i < totalPalabras ; i++) {
@@ -77,19 +79,27 @@ void lee_archivo (char *nombre_archivo) {
    }
 
    // Calcula la probabilidad de aparición para cada palabra
-   for (int m = 0 ; m < totalPalabras ; m++) 
+   for (int m = 0 ; m < totalPalabras ; m++){ 
       vec_pal[m].probabilidad = (double)vec_pal[m].frecuencia / totalFrecuenciaPalabras;
+      if(vec_pal[m].probabilidad != 0){
+         entropia+=vec_pal[m].probabilidad*log2(1/vec_pal[m].probabilidad);
+         longitudMedia+=strlen(vec_pal[m].palabra)*vec_pal[m].probabilidad;
+      }
+   }
 
     // Imprime las palabras y sus probabilidades
    for (int n = 0 ; n < totalPalabras ; n++) 
-      printf("Palabra: %s, Frecuencia: %d, Probabilidad: %.4f\n", vec_pal[n].palabra, vec_pal[n].frecuencia, vec_pal[n].probabilidad);
+      printf("Palabra: %s\t Frecuencia: %d\t Probabilidad: %.4f\n", vec_pal[n].palabra, vec_pal[n].frecuencia, vec_pal[n].probabilidad);
    
+
+   printf("\nLa entropia de la fuente es: %.2f bits\n", entropia);
+   printf("La longitud media del codigo es :%.2f\n", longitudMedia);
    //Imprime el alfabeto codigo
-   printf("--- Alfabeto codigo ---\n");
+   printf("\n--- Alfabeto codigo ---\n");
    
    for (k = 0 ; k < MAX_ALFABET ; k++) 
       if (alfabeto[k])
-      printf("Componente %d  =  %c \n", k,(char)k);
+      printf("Componente %d:  %c \n", k,(char)k);
    
    fclose(archivo);
 }
